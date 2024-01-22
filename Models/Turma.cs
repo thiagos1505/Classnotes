dd5ba2f3ccb7ac552be0f06c2264654a376d8af3
thiagos1505 <thiagosingulano@hotmail.com>
2024-01-22 00:01:51 -0300

    feat: adiciona a entidade de turma

---
+++ Models/Turma.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Classnotes.Models
+{
+    public class Turma
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+
+        public string? Turno { get; set; }
+
+        public int Ano { get; set; }
+
+        public Professor Professor { get; set; }
+    }
+}
