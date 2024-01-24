30eed48822617249398b87c6eabfe00a469a68a6
thiagos1505 <thiagosingulano@hotmail.com>
2024-01-23 22:31:34 -0300

    feat: adiciona as entidades de turma e aluno e define algumas annotations

---
+++ Models/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Classnotes.Models
+{
+    [Table("Alunos")]
+    public class Aluno
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo sobrenome é obrigatório")]
+        public string? Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "O campo data de nascimento é obrigatório")]
+        [Display(Name = "Data de Nascimento")]
+        public string? DataNascimento { get; set; }
+
+        public Turma Turma { get; set; }
+
+
+    }
+}
--- Models/Turma.cs
+++ Models/Turma.cs
@@ -1,17 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Humanizer;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Classnotes.Models
 {
+    [Table("Turmas")]
     public class Turma
     {
         [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
         public string? Nome { get; set; }
 
-        public string? Turno { get; set; }
+        [Required(ErrorMessage = "O campo turno é obrigatório")]
+        public Turno turno { get; set; }
 
+        [Required(ErrorMessage = "O campo ano é obrigatório")]
         public int Ano { get; set; }
 
-        public Professor Professor { get; set; }
+        public int ProfessorId { get; set; }
     }
 }
+    public enum Turno
+    {
+        Manha,
+        Tarde
+    }
+
